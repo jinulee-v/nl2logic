0adbc4ba2e99c8d1d44bd76fd3d8699ac234191a
Jinu Lee <jinulee@icgpu01.openstacklocal>
2024-10-16 14:20:17 -0500

    fix: make batch eval run background

--- scripts/ssa_eval.sh
+++ scripts/ssa_eval.sh
@@ -3,6 +3,6 @@ for genmode in "beam_size1" "beam_size16" "sample_size16_temp1.0"
 do
     for dataset in "entailmentbank_train" "entailmentbank_validation" "enwn_validation" "eqasc_train" "eqasc_validation" "prontoqa_train" "prontoqa_validation"
     do
-        python evaluate_predictions.py --data_prefix results/$1/${genmode}/${dataset}
+        python evaluate_predictions.py --data_prefix results/$1/${genmode}/${dataset} &
     done
 done
